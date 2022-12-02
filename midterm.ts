3aef00a254e40862cee19f5377f83999ba2f5881
Kyle Dominic Lomigo <91366618+KyleDom@users.noreply.github.com>
2022-12-02 21:20:47 +0800

    Update midterm.ts

--- midterm.ts
+++ midterm.ts
@@ -7,5 +7,22 @@ const date2 = new Date("December 1, 2022 07:35:00")
 console.log(diffOfTwoDates(date1, date2))
 
 
+class Player {
+    firstName : string;
+    lastName : string; 
+    number : number;
+    age : number;
+    constructor(firstName: string, lastName : string, number : number, age : number){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.number = number;
+        this.age = age; 
+    }
+}
+
+const player1 = new Player("Lebron", "James", 23, 39)
+const player2 = new Player("Davis", "Anthony", 6, 29)
+const player3 = new Player("Westbrook", "Russ", 0, 35)
+let playersArray = [player1, player2, player3]
 
 
